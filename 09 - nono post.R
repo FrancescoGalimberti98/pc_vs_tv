c4d3bcd95f95719db60f46b143fdfb996cc0f8c7
Francesco Galimberti <francescogalimberti98@gmail.com>
2022-07-20 10:28:12 +0200

    Modifiche script

--- 09 - nono post.R
+++ 09 - nono post.R
@@ -14,12 +14,22 @@ data$Zona = factor(data$Zona, levels = c('Nord Est','Nord Ovest','Centro','Isole
 beta = lm(pc_everyday ~ tv_everyday, data = data)$coefficients
 cor(data$pc_everyday,data$tv_everyday)
 
+# glm.data = data.frame(Regione = data$Regione,
+#                       pc = data$pc_everyday*10,
+#                       no_pc = 1000 - data$pc_everyday*10,
+#                       tv = data$tv_everyday)
+# y = as.matrix(data.frame(pc = glm.data$pc,no_pc = glm.data$no_pc))
+# 
+# fit.glm = glm(y ~ tv, data = glm.data, family = 'binomial')
+# prev.glm = predict(fit.glm, newdata = data.frame(tv = seq(0,1,by=0.001)))
+# plot(seq(0,1,by=0.001), prev.glm, type  = 'l')
+
 ggplot(data, aes(y=pc_everyday, x = tv_everyday, color = Zona)) +
   geom_point(size=3,show.legend = F) +
   #ylim(28.2,41.9) +
   geom_text_repel(aes(label = Regione),show.legend = F,
                   family = 'Montserrat',fontface = "bold",size = 8, min.segment.length = 3,point.padding = unit(1,"lines")) +
-  labs(y = "% pc_everyday", x = "% tv_everyday",family = 'Montserrat',fontface = "bold") +
+  labs(y = "% uso quotidiano PC", x = "% uso quotidiano TV",family = 'Montserrat',fontface = "bold") +
   scale_color_viridis(begin = 0.25,end = 0.83,direction = 1,discrete = TRUE, option = "G") +
   theme_minimal(base_size=34, base_family = 'Montserrat') +
   theme(axis.line = element_line(color='grey85'), panel.grid.minor = element_blank(),
